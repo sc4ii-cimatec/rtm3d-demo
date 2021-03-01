6703c9e226df88b6ad1b2486361210e563fed32d
Anderson Amorim <anderson.nascimento@fieb.org.br>
2021-03-01 16:04:18 -0300

    Updated initenv.sh to download from CIMATEC's ftp server

--- initenv.sh
+++ initenv.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-
+USER=xilinx.fpga 
+PASSKEY="W39@Ld&"
 ROOTDIR=`pwd`
-daturl="https://ucc44bb8f1bdd85e039bee9420cb.dl.dropboxusercontent.com/cd/0/get/BJuRvOuXE8wEiALjizy8JMr81XD_iSnS8qH2lvepOhMnFxCvikPvZubaRxZY263xKiSiMG03UoDJHG5g6JsIFRfpMDaDEVmun6nrdqzQ9OdpLP1Iq2XtgIAAD15WeHdFz8A/file?_download_id=52453586887120844318846471753034249484744742225488098457927462395&_notify_domain=www.dropbox.com&dl=1"
-datfile="rtm3d-demo-data.tar.xz"
-datdir="$ROOTDIR/data"
-mkdir -p $datdir
-cd $datdir
+DATURL="ftp://200.9.65.22/rtm3d-demo-data.tar.xz"
+DATFILE="rtm3d-demo-data.tar.xz"
+DATDIR="$ROOTDIR/data"
+mkdir -p $DATDIR
+cd $DATDIR
 echo "> Initializing DEMO environment..."
 echo "> Downloading demonstration data package..."
-wget --no-check-certificate $daturl -O $datfile
+echo "wget $DATURL --user=$USER --password=$PASSKEY"
+wget $DATURL --user=$USER --password=$PASSKEY
 
 echo "> Extracting demonstration data..."
-tar -xvf $datfile 
+tar -xvf $DATFILE 
 
 echo "> Build CWP library for nplot command. Please, answer 'y' when prompted."
-cd $datdir/cwp 
+cd $DATDIR/cwp 
 ./buildcwp.sh -y
 
 cd $ROOTDIR
